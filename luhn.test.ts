9d2ace419695aa5c9cdea5c4c4f3dbbf31550f49
Lucas Oliveira <hub.lucasoliveira@gmail.com>
2020-10-01 21:23:43 -0300

    fixup! Update Luhn.valid static method

--- luhn.test.ts
+++ luhn.test.ts
@@ -25,23 +25,23 @@ describe("Luhn", () => {
     expect(Luhn.valid("8273 1232 7352 0569")).toBeFalsy()
   })
 
-  xit("valid strings with a non-digit included become invalid", () => {
+  it("valid strings with a non-digit included become invalid", () => {
     expect(Luhn.valid("055a 444 285")).toBeFalsy()
   })
 
-  xit("valid strings with punctuation included become invalid", () => {
+  it("valid strings with punctuation included become invalid", () => {
     expect(Luhn.valid("055-444-285")).toBeFalsy()
   })
 
-  xit("valid strings with symbols included become invalid", () => {
+  it("valid strings with symbols included become invalid", () => {
     expect(Luhn.valid("055£ 444$ 285")).toBeFalsy()
   })
 
-  xit("single zero with space is invalid", () => {
+  it("single zero with space is invalid", () => {
     expect(Luhn.valid(" 0")).toBeFalsy()
   })
 
-  xit("input digit 9 is correctly converted to output digit 9", () => {
+  it("input digit 9 is correctly converted to output digit 9", () => {
     expect(Luhn.valid("091")).toBeTruthy()
   })
 })

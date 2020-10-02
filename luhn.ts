300f05f0dc17a8a62f5dc01fa507d217c4289fdb
Lucas Oliveira <hub.lucasoliveira@gmail.com>
2020-10-01 22:14:17 -0300

    Refactory luhn module
    - valid method do so many things
    
    Extract each part of the method in functions with just one responsability

--- luhn.ts
+++ luhn.ts
@@ -1,27 +1,39 @@
 export default class Luhn {
   static valid(input: string) {
-    const normalized = input.replace(/\s/g, "")
+    const normalized = Luhn.normalize(input)
 
     if (normalized.length <= 1) {
       return false
     }
 
+    return Luhn.sum(normalized) % 10 === 0
+  }
+
+  static normalize(input: string) {
+    return input.replace(/\s/g, "")
+  }
+
+  static sum(input: string) {
     let sum = 0
 
-    for (let i = 0; i < normalized.length; i++) {
-      let value = Number(normalized[normalized.length - i - 1])
-
-      if (i % 2 !== 0) {
-        value *= 2
-      }
-
-      if (value > 9) {
-        value -= 9
-      }
-
-      sum += value
+    for (let i = 0; i < input.length; i++) {
+      sum += Luhn.valueIn(input, i)
     }
 
-    return sum % 10 === 0
+    return sum
+  }
+
+  static valueIn(input: string, i: number) {
+    let value = Number(input[input.length - i - 1])
+
+    if (i % 2 !== 0) {
+      value *= 2
+    }
+
+    if (value > 9) {
+      value -= 9
+    }
+
+    return value
   }
 }
